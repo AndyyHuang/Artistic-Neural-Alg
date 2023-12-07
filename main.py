b4ddd7f4bf67c8be9932bb5eb9284d16e2c3d223
Andyyhuang <handy1221@berkeley.edu>
2023-12-06 16:14:28 -0800

    done

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ vgg19 = models.vgg19(weights=models.VGG19_Weights.DEFAULT)
 
 # Import images
 content, style, input = import_images("content/dog_with_stick.jpg", "style/femme.jpg")
-output_dir_path = "output_c1/picasso_dog"
+output_dir_path = "output_32131/picasso_dog"
 # input = torch.clone(content)
 input.requires_grad_(True)
 optimizer = torch.optim.LBFGS([input])
@@ -24,13 +24,12 @@ content_layer = 1
 style_layer = 5
 
 # Get model and losses
-content, style, input = import_images("content/dog_with_stick.jpg", "style/femme.jpg")
 model, content_losses, style_losses = create_neural_model(vgg19, content, style)
 model.eval()
 model.requires_grad_(False)
 
 # Logger
-# writer = SummaryWriter(f'stats/run_2/{output_dir_name}')
+writer = SummaryWriter(f'stats/{output_dir_path}')
 
 # Save final result
 output = train_image(input, model, optimizer, epochs, w_c, w_s, content_losses, style_losses, content_layer, style_layer, output_dir_path, writer=None, log=True)
